d50ec5083c3f11aac3192ca69b1aeabc65761e4b
bob joe <matthew@krupczak.org>
2023-05-30 15:12:30 -0400

    fix small typo in user prompt

--- ask_your_document.py
+++ ask_your_document.py
@@ -21,7 +21,7 @@ def main():
     api_key = args.key or DEFAULT_OPENAI_API_KEY
     if api_key == 'YOUR_OPENAI_API_KEY':
         print("You must replace 'YOUR_OPENAI_API_KEY' with your actual OpenAI API key in the script, or provide it using the --key flag.")
-        print("For example: python3 ask_your_document.py --key 'document.pdf 'What is the title of this document?'")
+        print("For example: python3 ask_your_document.py --key 'YOUR_OPENAI_API_KEY' 'document.pdf 'What is the title of this document?'")
         return
 
     os.environ["OPENAI_API_KEY"] = api_key
